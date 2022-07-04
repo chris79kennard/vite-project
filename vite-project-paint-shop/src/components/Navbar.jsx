b170d9514e23a52449f0512ebe18f0bb8280e401
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2022-07-04 15:07:26 -0500

    in line comments added for future reference.

--- vite-project-paint-shop/src/components/Navbar.jsx
+++ vite-project-paint-shop/src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
 
-
+// Function for handling the Navigation bar 
 function Navbar() {
+  // Declaring Click and providing a value as default false.
   const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!click)
+  // After user clicks the Hamburger icon to open the NavBar this is whats returned~
   return (
     <>
     <nav className='navbar'>
       <div className='navbar-container'>
         <Link to='/' className='navbar-logo'>
-          P.B.K <i className='fab fa-typo3'></i>
+          P.B.K 
+          <i className='fab fa-typo3'></i>
         </Link>
-        <div className='menu-icon'>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'}>
-          </i>
+        <div className='menu-icon' onClick={handleClick}>
+          {/* after user clicks hamburger it turns to an X icon */}
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
         </div>
+        {/* after user clicks this removes dropdown menu */}
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul>
       </div>
 
     </nav>
